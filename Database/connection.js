1e91c26c0f507c2218961cade129c5e5e7a40565
dont touch my mac <vishal@vishals-MacBook-Pro.local>
2023-09-13 19:37:32 +0530

    commit

--- Database/connection.js
+++ Database/connection.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 
   
 module.exports = async ()=>{
-    return mongoose.connect("mongodb+srv://vishal0119:oJu0GphuAdX3UYNn@cluster0.vf4j70r.mongodb.net/fakeStoreApi");
+    return await mongoose.connect("mongodb+srv://vishal0119:oJu0GphuAdX3UYNn@cluster0.vf4j70r.mongodb.net/fakeStoreApi");
 };
